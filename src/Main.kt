4fa694bf6d1a1a19a6abc6226186795ffad9ee0e
P466392 <P466392@tafe.wa.edu.au>
2025-08-01 10:26:21 +0800

    Added calculateCoffeeLevel function

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    calculateCaffeeLevel()
+}
+
+fun calculateCaffeeLevel() {
     print("How many hours have you currently worked: ")
     var hoursDone = readLine()!!.toInt()
     print("How many more hours did you need to work: ")
     var hoursMore = readLine()!!.toInt()
-    print("How tried are you? ")
-    println("(Energetic, Okay, Tired, KO)")
-    var tiredLevel = readLine()!!.toString()
-
+    var tiredLevel = "null"
+    while (tiredLevel == "null") {
+        print("How tried are you? ")
+        println("(Energetic, Okay, Tired, KO)")
+        tiredLevel = readLine()!!
+        if (tiredLevel == "Energetic") {
+            continue
+        } else if (tiredLevel == "Okay") {
+            continue
+        } else if (tiredLevel == "Tired") {
+            continue
+        } else if (tiredLevel == "KO") {
+            tiredLevel = "KO"
+        } else {
+            println("Please enter the following options: Energetic, Okay, Tired, KO")
+            tiredLevel = "null"
+        }
+    }
     println(tiredLevel)
     println(hoursMore)
     println(hoursDone)
